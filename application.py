3aeff94c36d2d95d19579cef925b149034bfc330
Michelle Espranita <michespranita@gmail.com>
2020-04-30 23:37:49 +0200

    Messages view and sending messages implemented, but there are still bugs

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 from flask import Flask, session, request, render_template, url_for, redirect, flash, jsonify
 from flask_socketio import SocketIO, emit
 
@@ -8,7 +8,8 @@ SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
 socketio = SocketIO(app)
 
-channels = {} # contains tuples of (name, desc)
+channels = {} # contains key-value pair of (name, desc)
+conversations = {} # contains key-value pair of (name, dataframe)
 hidden = True
 showWarning = False
 
@@ -20,6 +21,7 @@ def index():
 def home():
     global hidden
     global showWarning
+    print(hidden, showWarning)
     return render_template("home.html", channels=channels, hidden=hidden, showWarning=showWarning)
 
 @app.route("/create-channel", methods=["POST"])
@@ -40,5 +42,25 @@ def createChannel():
         hidden = True
         showWarning = False
         return redirect(url_for('home'))
+
+@app.route("/get-messages", methods=["GET"])
+def getMessages():
+    global conversations
+    channelName = request.form.get('channel_name')
+    return conversations[channelName]
+
+@socketio.on('send message')
+def send_message(data):
+    global conversations
+    channel_name = data['channel_name']
+    message_time = data['message_time']
+    message_sender = data['message_sender']
+    message_text = data['message_text']
+    if channel_name not in conversations:
+        conversations[channel_name] = [{'message_text': message_text, 'message_sender': message_sender, 'message_time': message_time}]
+    else:
+        conversations[channel_name].append({'message_text': message_text, 'message_sender': message_sender, 'message_time': message_time})
+    emit('current messages', conversations, broadcast=True)
+
 
       
