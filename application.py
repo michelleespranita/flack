f60967f1ad89ce1074051455992a03fec91cc4a6
Michelle Espranita <michespranita@gmail.com>
2020-05-05 01:15:09 +0200

    Reply to message function

--- application.py
+++ application.py
@@ -10,8 +10,11 @@ socketio = SocketIO(app)
 
 channels = {} # list of (name, desc)
 conversations = {} # contains key-value pair of (name, list of dictionaries containing chats)
+# Ex: {'Mario Kart': [{'message_id': 1, 'message_sender': 'Michelle', 'message_text': 'Hahaha', 'message_time': '2020', 'reply_id': 0}, ...], ...}
+# 0 is reserved for nothing
 hidden = True
 showWarning = False
+global_id = 1
 
 @app.route("/", methods=["GET"])
 def index():
@@ -52,7 +55,6 @@ def getMessages():
     global conversations
     channelName = request.form.get('channel_name')
     if channelName in conversations.keys():
-        print(conversations[channelName])
         return jsonify(conversations[channelName])
     else:
         return jsonify([])
@@ -69,16 +71,20 @@ def createChannel(data):
 @socketio.on('send message')
 def send_message(data):
     global conversations
+    global global_id
     channel_name = data['channel_name']
     message_time = data['message_time']
     message_sender = data['message_sender']
     message_text = data['message_text']
+    reply_message_id = int(data['reply_id'])
     if channel_name not in conversations.keys():
-        conversations[channel_name] = [{'message_text': message_text, 'message_sender': message_sender, 'message_time': message_time}]
+        conversations[channel_name] = [{'message_id': global_id, 'message_text': message_text, 'message_sender': message_sender, 'message_time': message_time, 'reply_id': reply_message_id}]
     else:
         if len(conversations[channel_name]) >= 100:
             del conversations[channel_name][0]
-        conversations[channel_name].append({'message_text': message_text, 'message_sender': message_sender, 'message_time': message_time})
+        conversations[channel_name].append({'message_id': global_id, 'message_text': message_text, 'message_sender': message_sender, 'message_time': message_time, 'reply_id': reply_message_id})
+    global_id += 1
+    print(conversations)
     emit('current messages', conversations, broadcast=True)
 
 
